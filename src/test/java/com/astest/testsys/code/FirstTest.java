cafe9dad0b94855efab2d4dc3e1ad41182ca4eba
zhaoning01 <zhaoning01@missfresh.cn>
2020-05-06 18:30:27 +0800

    初始化

--- src/test/java/com/astest/testsys/code/FirstTest.java
+++ src/test/java/com/astest/testsys/code/FirstTest.java
@@ -1,11 +1,9 @@
-package com.astest.testsys.utils;
+package com.astest.testsys.code;
 
 /**
  * @author: zhaoning01
  * Date: 2020/5/6
  * Description: xxxx
  */
-public class FirstLet {
-
-
+public class FirstTest {
 }
